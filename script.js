4dde53fec22027bbb7728868271895fd983d79cc
Khaliun <khaliunsaya@gmail.com>
2022-12-23 15:20:38 -0800

    initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+var planet={
+
+  name:"Earth",
+  age: "4.543 billion years", 
+  moons: 1,
+  isPopulated: true,
+  population: "7.594 billion"
+}
+
+
+
+console.log(planet.name);
+console.log(planet["name"]); 
